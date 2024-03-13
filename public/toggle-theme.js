fd9f8e24a2a92aa6c4f497b4c063ef69c55c6e99
liruifengv <liruifeng1024@gmail.com>
2024-03-13 19:31:45 +0800

    fix: fix theme bug

--- public/toggle-theme.js
+++ public/toggle-theme.js
@@ -3,25 +3,24 @@ function sendThemeChange(themeValue) {
 }
 
 const getThemePreference = () => {
-  if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) {
-    return localStorage.getItem('theme');
+  if (window.localStorage.getItem('theme')) {
+    return window.localStorage.getItem('theme');
   }
   return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
 };
 
 window.onload = ()=> {
   const isDark = getThemePreference() === 'dark';
-  alert(isDark)
   document.documentElement.classList[isDark ? 'add' : 'remove']('theme-dark');
   sendThemeChange(getThemePreference());
 
-  if (typeof localStorage !== 'undefined') {
-    // Watch the document element and persist user preference when it changes.
-    const observer = new MutationObserver(() => {
-      const isDark = document.documentElement.classList.contains('theme-dark');
-      sendThemeChange(isDark ? 'dark' : 'light');
-      localStorage.setItem('theme', isDark ? 'dark' : 'light');
-    });
-    observer.observe(document.documentElement, { attributes: true, attributeFilter: ['class'] });
-  }
+  // Watch the document element and persist user preference when it changes.
+  const observer = new MutationObserver(() => {
+    const isDark = document.documentElement.classList.contains('theme-dark');
+    sendThemeChange(isDark ? 'dark' : 'light');
+    window.localStorage.setItem('theme', isDark ? 'dark' : 'light');
+  });
+  observer.observe(document.documentElement, { attributes: true, attributeFilter: ['class'] });
+
+  document.documentElement.classList.add('loaded');
 }
