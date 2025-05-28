291b5c1f8c7d4f4071bb27d29afff51d03160130
liruifengv <liruifeng1024@gmail.com>
2025-05-28 17:20:33 +0800

    添加友链

--- src/links.ts
+++ src/links.ts
@@ -1,156 +1,168 @@
 export interface Link {
-	name: string;
-	href: string;
-	logo: string
-  darkLogo?: string
-  description?: string
+  name: string;
+  href: string;
+  logo: string;
+  darkLogo?: string;
+  description?: string;
 }
 
 export const links: Link[] = [
   {
-    name: 'Kai',
-    href: 'https://kaiyi.cool',
-    logo: 'kaiyi.cool.webp',
-    description: 'Web Worker 播客主播，Qwerty Learner 作者'
+    name: "Kai",
+    href: "https://kaiyi.cool",
+    logo: "kaiyi.cool.webp",
+    description: "Web Worker 播客主播，Qwerty Learner 作者",
   },
   {
-    name: '辛宝',
-    href: 'https://ijust.cc',
-    logo: 'ijust.cc.png',
-    description: 'WebWorker 播客主理人'
+    name: "辛宝",
+    href: "https://ijust.cc",
+    logo: "ijust.cc.png",
+    description: "WebWorker 播客主理人",
   },
   {
-    name: 'Fox',
-    href: 'https://mksaas.me',
-    logo: 'mksaas.me.png',
-    description: 'Indie Maker. Founder of Mksaas'
+    name: "Fox",
+    href: "https://mksaas.me",
+    logo: "mksaas.me.png",
+    description: "Indie Maker. Founder of Mksaas",
   },
   {
-    name: '面条',
-    href: 'https://miantiao.me',
-    logo: 'miantiao.me.png',
-    darkLogo: 'miantiao.me-dark.webp',
-    description: 'Open Source Enthusiast & Founder of sink.cool'
+    name: "面条",
+    href: "https://miantiao.me",
+    logo: "miantiao.me.png",
+    darkLogo: "miantiao.me-dark.webp",
+    description: "Open Source Enthusiast & Founder of sink.cool",
   },
   {
-    name: '且听书吟',
-    href: 'https://yufan.me',
-    logo: 'yufan.me.svg',
-    darkLogo: 'yufan.me-dark.svg',
-    description: '诗与梦想的远方'
+    name: "且听书吟",
+    href: "https://yufan.me",
+    logo: "yufan.me.svg",
+    darkLogo: "yufan.me-dark.svg",
+    description: "诗与梦想的远方",
   },
   {
-    name: 'Rico',
-    href: 'https://www.ricoui.com',
-    logo: 'ricoui.com.png',
-    description: '一名有趣的写代码的设计师'
+    name: "Rico",
+    href: "https://www.ricoui.com",
+    logo: "ricoui.com.png",
+    description: "一名有趣的写代码的设计师",
   },
   {
-    name: 'Innei 拾一',
-    href: 'https://innei.in',
-    logo: 'innei.in.webp',
-    description: '全栈开发工程师'
+    name: "Innei 拾一",
+    href: "https://innei.in",
+    logo: "innei.in.webp",
+    description: "全栈开发工程师",
   },
   {
-    name: '宇航',
-    href: 'https://www.yuhang.dev',
-    logo: 'yuhang.dev.png',
-    description: 'Web 开发者｜现居良渚｜家有四猫｜运动爱好者'
+    name: "宇航",
+    href: "https://www.yuhang.dev",
+    logo: "yuhang.dev.png",
+    description: "Web 开发者｜现居良渚｜家有四猫｜运动爱好者",
   },
   {
-    name: '皮普的数字花园',
-    href: 'https://pipuwong.com',
-    logo: 'https://img.pipu.studio/kCchCV.jpg',
-    description: '设计/摄影/代码/NAS'
+    name: "皮普的数字花园",
+    href: "https://pipuwong.com",
+    logo: "https://img.pipu.studio/kCchCV.jpg",
+    description: "设计/摄影/代码/NAS",
   },
   {
-    name: 'Simon He',
-    href: 'https://simonhe.me',
-    logo: 'simonhe.me.jpeg',
-    description: 'core member of unocss and vue vine, webview-use author'
+    name: "Simon He",
+    href: "https://simonhe.me",
+    logo: "simonhe.me.jpeg",
+    description: "core member of unocss and vue vine, webview-use author",
   },
   {
-    name: 'Justin3go',
-    href: 'https://justin3go.com',
-    logo: 'justin3go.com.jpg',
-    description: 'Indie Hacker & Founder of template0.com'
+    name: "Justin3go",
+    href: "https://justin3go.com",
+    logo: "justin3go.com.jpg",
+    description: "Indie Hacker & Founder of template0.com",
   },
   {
-    name: 'Weijun',
-    href: 'https://weijunext.com/',
-    logo: 'weijunext.com.jpg',
-    description: 'Indie Hacker & Founder of nextjscn.org'
+    name: "Weijun",
+    href: "https://weijunext.com/",
+    logo: "weijunext.com.jpg",
+    description: "Indie Hacker & Founder of nextjscn.org",
   },
   {
-    name: '连波',
-    href: 'https://godruoyi.com',
-    logo: 'godruoyi.com.webp',
-    description: 'Laravel 爱好者，Blogger'
+    name: "连波",
+    href: "https://godruoyi.com",
+    logo: "godruoyi.com.webp",
+    description: "Laravel 爱好者，Blogger",
   },
   {
-    name: '但为君故',
-    href: 'https://dreams.plus',
-    logo: 'https://dreams.plus/static/avatar.webp',
-    description: 'Z世代青年'
+    name: "但为君故",
+    href: "https://dreams.plus",
+    logo: "https://dreams.plus/static/avatar.webp",
+    description: "Z世代青年",
   },
   {
-    name: '陈随易',
-    href: 'https://chensuiyi.me',
-    logo: 'chensuiyi.me.png',
-    description: '前端之虎，农村程序员，独立开发者'
+    name: "陈随易",
+    href: "https://chensuiyi.me",
+    logo: "chensuiyi.me.png",
+    description: "前端之虎，农村程序员，独立开发者",
   },
   {
-    name: 'situ2001',
-    href: 'https://situ2001.com',
-    logo: 'https://situ2001.com/avatar.png',
-    description: '软件开发者，Blogger'
+    name: "situ2001",
+    href: "https://situ2001.com",
+    logo: "https://situ2001.com/avatar.png",
+    description: "软件开发者，Blogger",
   },
   {
-    name: 'AprilNEA',
-    href: 'https://blog.sku.moe',
-    logo: 'blog.sku.moe.jpg',
-    description: 'Full stack engineer / Student'
+    name: "AprilNEA",
+    href: "https://blog.sku.moe",
+    logo: "blog.sku.moe.jpg",
+    description: "Full stack engineer / Student",
   },
   {
-    name: 'SkyWT',
-    href: 'https://skywt.cn',
-    logo: 'skywt.cn.webp',
-    description: '软件工程师，黑客与画家。'
+    name: "SkyWT",
+    href: "https://skywt.cn",
+    logo: "skywt.cn.webp",
+    description: "软件工程师，黑客与画家。",
   },
   {
-    name: '姚利锋 | Immerse',
-    href: 'https://yaolifeng.com',
-    logo: 'yaolifeng.com.jpg',
-    description: '独立开发者、内容创作者。'
+    name: "姚利锋 | Immerse",
+    href: "https://yaolifeng.com",
+    logo: "yaolifeng.com.jpg",
+    description: "独立开发者、内容创作者。",
   },
   {
-    name: 'Corey Chiu',
-    href: 'https://coreychiu.com',
-    logo: 'coreychiu.com.jpg',
-    description: '独立开发者、内容创作者。'
+    name: "Corey Chiu",
+    href: "https://coreychiu.com",
+    logo: "coreychiu.com.jpg",
+    description: "独立开发者、内容创作者。",
   },
   {
-    name: 'Leetao',
-    href: 'https://www.leetao.me',
-    logo: 'leetao.me.jpg',
-    description: '后端工程师，写有趣的代码，做有趣的事'
+    name: "Leetao",
+    href: "https://www.leetao.me",
+    logo: "leetao.me.jpg",
+    description: "后端工程师，写有趣的代码，做有趣的事",
   },
   {
-    name: '希仁之拥',
-    href: 'https://blog.keepchen.com',
-    logo: 'blog.keepchen.com.jpg',
-    description: 'Indie Maker & Full-Stack Dev'
+    name: "希仁之拥",
+    href: "https://blog.keepchen.com",
+    logo: "blog.keepchen.com.jpg",
+    description: "Indie Maker & Full-Stack Dev",
   },
   {
-    name: 'Jimmy',
-    href: 'https://about.jimmy-blog.top',
-    logo: 'jimmy-blog.top.png',
-    description: '全栈开发工程师'
+    name: "Jimmy",
+    href: "https://about.jimmy-blog.top",
+    logo: "jimmy-blog.top.png",
+    description: "全栈开发工程师",
   },
   {
-    name: 'Aaron Conlon',
-    href: 'https://i5lin.top',
-    logo: 'i5lin.top.jpeg',
-    description: '全栈开发工程师'
-  }
-]
+    name: "Aaron Conlon",
+    href: "https://i5lin.top",
+    logo: "i5lin.top.jpeg",
+    description: "全栈开发工程师",
+  },
+  {
+    name: "吐槽大王部落格",
+    href: "https://www.tcdw.net",
+    logo: "tcdw.net.webp",
+    description: "全栈开发工程师",
+  },
+  {
+    name: "OldPanda",
+    href: "https://old-panda.com",
+    logo: "old-panda.com.jpeg",
+    description: "南村群童欺我老无力",
+  },
+];
